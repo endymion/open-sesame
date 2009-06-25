ae19baa479797d1483f4efc9e81c9c6c3cbdc43d
Ryan Alyn Porter <rap@endymion.com>
2009-06-25 12:02:01 -0400

    Improved application integration for real-world use.

---
+++ lib/open-sesame.rb
@@ -0,0 +1,17 @@
+require 'digest/sha1'
+
+module OpenSesame
+  class Token
+    @default_secret = "OPEN SESAME"
+    def self.generate(timestamp = DateTime.now, secret = @default_secret)
+      timestamp_string = timestamp.strftime('%Y%m%dT%H%M')
+      hash = (Digest::SHA1.new << secret + timestamp_string).to_s
+      timestamp_string + '-' + hash
+    end
+    def self.verify(token, secret = @default_secret)
+      string = token.split /-/
+      timestamp = DateTime.strptime string[0], '%Y%m%dT%H%M'
+      timestamp - DateTime.now <= 1.hour and token.eql? generate(timestamp)
+    end
+  end
+end
