685c1107109c124a2fabca06765d6fa7eacdd611
dwaite <david@alkaline-solutions.com>
2009-06-25 13:40:28 +0800

    Prefix the hash with the secret to make certain attacks harder Put timestamp first in the token to make reading easier Verify timestamp before doing any hash checking Reuse generate function to validate token (the same token and secret should generate an identical value)
    Signed-off-by: endymion <rap@endymion.com>

--- lib/open_sesame.rb
+++ lib/open_sesame.rb
@@ -3,18 +3,14 @@ require 'digest/sha1'
 module OpenSesame
   class Token
     @secret = "OPEN SESAME"
-    def self.generate
-      timestamp = DateTime.now.strftime('%m%d%Y%H%M')
-      hash = (Digest::SHA1.new << timestamp + @secret).to_s
-      hash + '-' + timestamp
+    def self.generate(timestamp = DateTime.now)
+      hash = (Digest::SHA1.new << @secret + timestamp.strftime('%Y%m%dT%H%M')).to_s
+      timestamp + '-' + hash
     end
     def self.verify(token)
       string = token.split /-/
-      timestamp = (DateTime.strptime string[1], '%m%d%Y%H%M')
-      hash = (Digest::SHA1.new << timestamp.strftime('%m%d%Y%H%M') + @secret).to_s
-      return true if timestamp - DateTime.now <= 1.hour and
-        string[0].eql? hash
-      false
+      timestamp = DateTime.strptime string[0], '%Y%m%dT%H%M'
+      timestamp - DateTime.now <= 1.hour and token.eq? generate(timestamp)
     end
   end
 end
